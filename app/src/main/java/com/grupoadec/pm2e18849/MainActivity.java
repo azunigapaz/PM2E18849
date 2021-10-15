b3ced3db89202008fccb2d947410531558c456ca
Alejandro <azunigapaz@gmail.com>
2021-10-15 10:50:25 -0600

    Se acomodaron algunos objetos en el activity principal y en el activity de paises, se cambio el nombre del logo en minuscula

--- app/src/main/java/com/grupoadec/pm2e18849/MainActivity.java
+++ app/src/main/java/com/grupoadec/pm2e18849/MainActivity.java
@@ -2,13 +2,29 @@ package com.grupoadec.pm2e18849;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgagregarpais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgagregarpais = (ImageView) findViewById(R.id.imgagregarpais);
+
+        imgagregarpais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ActivityRegistroPaises.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
