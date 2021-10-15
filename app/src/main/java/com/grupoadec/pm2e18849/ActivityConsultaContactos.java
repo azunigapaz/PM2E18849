500252a6c44fcb6b52ccbc65b8840a9cb9a6df07
Alejandro <azunigapaz@gmail.com>
2021-10-15 08:28:23 -0600

    AddActivitiesPrincipales
    Se agrego los activitie principales, registro paises, consulta paises y consulta contactos

---
+++ app/src/main/java/com/grupoadec/pm2e18849/ActivityConsultaContactos.java
@@ -0,0 +1,14 @@
+package com.grupoadec.pm2e18849;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConsultaContactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_contactos);
+    }
+}
---
+++ app/src/main/java/com/grupoadec/pm2e18849/ActivityConsultaPaises.java
@@ -0,0 +1,14 @@
+package com.grupoadec.pm2e18849;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConsultaPaises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_paises);
+    }
+}
