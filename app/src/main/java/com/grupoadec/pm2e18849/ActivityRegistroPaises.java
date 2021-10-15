1c82b9f7b671718dface7ab8bd4746d240228ff3
josevigil27 <91038590+josevigil27@users.noreply.github.com>
2021-10-15 12:32:05 -0600

    Disenio consulta paises terminado

--- app/src/main/java/com/grupoadec/pm2e18849/ActivityRegistroPaises.java
+++ app/src/main/java/com/grupoadec/pm2e18849/ActivityRegistroPaises.java
@@ -2,13 +2,28 @@ package com.grupoadec.pm2e18849;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityRegistroPaises extends AppCompatActivity {
+
+    Button btnconsultapaises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_paises);
+
+        btnconsultapaises = (Button) findViewById(R.id.btnconsultapaises);
+
+        btnconsultapaises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ActivityConsultaPaises.class);
+                startActivity(intent);
+            }
+        });
     }
 }
