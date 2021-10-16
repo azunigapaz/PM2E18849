d8c21307698388f9f13f744167192254be6f2292
Alejandro <azunigapaz@gmail.com>
2021-10-15 22:08:54 -0600

    Accion pasar datos de ListView al activity de mantenimiento contacto terminado

--- app/src/main/java/com/grupoadec/pm2e18849/ActivityAccionContacto.java
+++ app/src/main/java/com/grupoadec/pm2e18849/ActivityAccionContacto.java
@@ -3,6 +3,7 @@ package com.grupoadec.pm2e18849;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.RestrictionEntry;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -17,26 +18,42 @@ import com.grupoadec.pm2e18849.Configuracion.Transacciones;
 public class ActivityAccionContacto extends AppCompatActivity {
 
     SQLiteConexion conexion;
-    EditText id, txtconsultanombrecontacto, txtconsultacelcontacto, txtconsultanotacontacto;
+    EditText txtconsultaidcontacto, txtconsultanombrecontacto, txtconsultacelcontacto, txtconsultanotacontacto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accion_contacto);
 
+
+        conexion = new SQLiteConexion(this,Transacciones.NameDatabase,null,1);
+
         Button btnllamarcontacto = (Button) findViewById(R.id.btnllamarcontacto);
         Button btnactualizarconsultacontacto = (Button) findViewById(R.id.btnactualizarconsultacontacto);
         Button btneliminarconsultacontacto = (Button) findViewById(R.id.btneliminarconsultacontacto);
 
-        id = (EditText) findViewById(R.id.txtconsultaidcontacto);
+        txtconsultaidcontacto = (EditText) findViewById(R.id.txtconsultaidcontacto);
         txtconsultanombrecontacto = (EditText) findViewById(R.id.txtconsultanombrecontacto);
         txtconsultacelcontacto = (EditText) findViewById(R.id.txtconsultacelcontacto);
         txtconsultanotacontacto = (EditText) findViewById(R.id.txtconsultanotacontacto);
 
+        // extramemos los put que vienen del activity anterior
+        String pxIdcontacto = getIntent().getStringExtra("pxIdcontacto");
+        String pxNombre = getIntent().getStringExtra("pxNombre");
+        String pxNumero = getIntent().getStringExtra("pxNumero");
+        String pxNota = getIntent().getStringExtra("pxNota");
+
+        // llenamos los EditText
+        txtconsultaidcontacto.setText(pxIdcontacto);
+        txtconsultanombrecontacto.setText(pxNombre);
+        txtconsultacelcontacto.setText(pxNumero);
+        txtconsultanotacontacto.setText(pxNota);
+
+
         btnllamarcontacto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (id.length() > 0 && txtconsultanombrecontacto.length() > 0 && txtconsultacelcontacto.length() > 0 && txtconsultanotacontacto.length() > 0) {
+                if (txtconsultaidcontacto.length() > 0 && txtconsultanombrecontacto.length() > 0 && txtconsultacelcontacto.length() > 0 && txtconsultanotacontacto.length() > 0) {
                     LlamarContacto();
                 } else {
                     Toast.makeText(getApplicationContext(), "Debe de llenar todos los campos para realizar la llamada",Toast.LENGTH_LONG).show();
@@ -47,8 +64,10 @@ public class ActivityAccionContacto extends AppCompatActivity {
         btnactualizarconsultacontacto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (id.length() > 0 && txtconsultanombrecontacto.length() > 0 && txtconsultacelcontacto.length() > 0 && txtconsultanotacontacto.length() > 0) {
+                if (txtconsultaidcontacto.length() > 0 && txtconsultanombrecontacto.length() > 0 && txtconsultacelcontacto.length() > 0 && txtconsultanotacontacto.length() > 0) {
+
                     ActualizarConsultaContacto();
+
                 } else {
                     Toast.makeText(getApplicationContext(), "Debe de llenar todos los campos para actualizar el contacto",Toast.LENGTH_LONG).show();
                 }
@@ -58,7 +77,7 @@ public class ActivityAccionContacto extends AppCompatActivity {
         btneliminarconsultacontacto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (id.length() > 0 && txtconsultanombrecontacto.length() > 0 && txtconsultacelcontacto.length() > 0 && txtconsultanotacontacto.length() > 0) {
+                if (txtconsultaidcontacto.length() > 0 && txtconsultanombrecontacto.length() > 0 && txtconsultacelcontacto.length() > 0 && txtconsultanotacontacto.length() > 0) {
                     EliminarConsultaContacto();
                 } else {
                     Toast.makeText(getApplicationContext(), "Debe de llenar todos los campos para eliminar el registro completo",Toast.LENGTH_LONG).show();
@@ -75,24 +94,26 @@ public class ActivityAccionContacto extends AppCompatActivity {
 
         SQLiteDatabase db = conexion.getWritableDatabase();
 
-        String [] params = { id.getText().toString() };
+        String [] params = { txtconsultaidcontacto.getText().toString() };
 
         ContentValues valores = new ContentValues();
         valores.put(Transacciones.nombrecontacto, txtconsultanombrecontacto.getText().toString());
         valores.put(Transacciones.numerocontacto, txtconsultacelcontacto.getText().toString());
         valores.put(Transacciones.notacontacto, txtconsultanotacontacto.getText().toString());
 
-        db.update(Transacciones.tablacontactos, valores, Transacciones.idpais + "=?", params);
+        db.update(Transacciones.tablacontactos, valores, Transacciones.idcontacto + "=?", params);
         Toast.makeText(getApplicationContext(), "Dato Actualizado", Toast.LENGTH_LONG).show();
+
         ClearScreen();
 
+        db.close();
     }
 
     private void EliminarConsultaContacto() {
 
         SQLiteDatabase db = conexion.getWritableDatabase();
 
-        String [] params = { id.getText().toString() };
+        String [] params = { txtconsultaidcontacto.getText().toString() };
 
         String wherecond = Transacciones.idcontacto + "=?";
 
@@ -105,7 +126,7 @@ public class ActivityAccionContacto extends AppCompatActivity {
 
     private void ClearScreen() {
 
-        id.setText("");
+        txtconsultaidcontacto.setText("");
         txtconsultanombrecontacto.setText("");
         txtconsultacelcontacto.setText("");
         txtconsultanotacontacto.setText("");
